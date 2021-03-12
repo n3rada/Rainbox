7e90b90fdaf76ed3be6d740ca61d60c9dbbd5446
n3rada <n3rada@pm.me>
2021-03-12 14:49:42 +0100

    yapf

--- rainbox.py
+++ rainbox.py
@@ -9,9 +9,10 @@ import sys
 ######################################
 ######################################
 
+
 def str2bool(v):
     if isinstance(v, bool):
-       return v
+        return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
@@ -19,11 +20,11 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+
 def parsing():
     parser = argparse.ArgumentParser(
-        formatter_class = argparse.RawTextHelpFormatter,
-        description = 
-        """
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="""
                             ██████╗  █████╗ ██╗███╗   ██╗██████╗  ██████╗ ██╗  ██╗
                             ██╔══██╗██╔══██╗██║████╗  ██║██╔══██╗██╔═══██╗╚██╗██╔╝
                             ██████╔╝███████║██║██╔██╗ ██║██████╔╝██║   ██║ ╚███╔╝ 
@@ -32,43 +33,52 @@ def parsing():
                             ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
                                      A generator of wifi router keys
                                              Author: n3rada
-        """
-    )
+        """)
 
     # nargs='?' because it's not obligatory
-    parser.add_argument(
-        '-c', '--cores',
-        type=int, nargs='?', default=mp.cpu_count(), help='Number of cores to use'
-        )
+    parser.add_argument('-c',
+                        '--cores',
+                        type=int,
+                        nargs='?',
+                        default=mp.cpu_count(),
+                        help='Number of cores to use')
+
+    parser.add_argument('-t',
+                        '--type',
+                        type=str,
+                        default="hexa",
+                        help="Alphabet type: hexa or alpha (for all)")
+
+    parser.add_argument('-a',
+                        '--alphabet',
+                        type=str,
+                        default=None,
+                        help="Alphabet case: low, up or mix")
+
+    parser.add_argument('-l',
+                        '--length',
+                        type=int,
+                        default=None,
+                        help="Ouput key size")
+
+    parser.add_argument("-w",
+                        '--wordlist',
+                        action='store_true',
+                        help="Wordlist output activated.")
+
+    parser.add_argument("-p",
+                        '--punctuation',
+                        action='store_true',
+                        help="Add punctuation to generation.")
 
     parser.add_argument(
-        '-t', '--type',
-        type=str, default="hexa", help="Alphabet type: hexa or alpha (for all)"
-    )
-
-    parser.add_argument(
-        '-a', '--alphabet',
-        type=str, default=None, help="Alphabet case: low, up or mix"
-    )
-
-    parser.add_argument(
-        '-l', '--length',
-        type=int, default=None, help="Ouput key size"
-    )
-
-    parser.add_argument(
-        "-w", '--wordlist',
-         action='store_true', help="Wordlist output activated."
-    )
-
-    parser.add_argument(
-        "-p", '--punctuation',
-         action='store_true', help="Add punctuation to generation."
-    )
-
-    parser.add_argument(
-        "-tf", '--toFind',
-        type=str, default=False, nargs="?", help="Enter the key to find here for testing the generator. Cannot be combined with wordlist !"
+        "-tf",
+        '--toFind',
+        type=str,
+        default=False,
+        nargs="?",
+        help=
+        "Enter the key to find here for testing the generator. Cannot be combined with wordlist !"
     )
 
     # If no arguments have been passed
@@ -78,21 +88,28 @@ def parsing():
     else:
         return parser.parse_args()
 
+
 def main():
-    
+
     args = parsing()
 
     try:
-        fragmentName = "FRAG_4_" + args.type.upper() + "-" + args.alphabet + "-" + str(args.punctuation)
+        fragmentName = "FRAG_4_" + args.type.upper(
+        ) + "-" + args.alphabet + "-" + str(args.punctuation)
     except TypeError:
         fragmentName = None
 
     t.clear(withAscii=True)
 
     try:
-        t.writer(keySize=args.length,cores=args.cores,fragName=fragmentName,wordlist=args.wordlist,keyToFind=args.toFind)
+        t.writer(keySize=args.length,
+                 cores=args.cores,
+                 fragName=fragmentName,
+                 wordlist=args.wordlist,
+                 keyToFind=args.toFind)
     except KeyboardInterrupt:
         sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
